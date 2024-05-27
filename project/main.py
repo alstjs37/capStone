660e8b88be13909406106ce452e3a6e1030828ec
alstjs37 <kubernetes.io@kakao.com>
2024-05-27 16:05:57 +0900

    chore: change port number

--- project/main.py
+++ project/main.py
@@ -59,4 +59,4 @@ def save_coordinates():
     return jsonify({'status': 'success', 'path': path_coordinates, 'length': shortest_path_length})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5002)
+    app.run(debug=True, host='0.0.0.0', port=5001)
